10aaa0420ffa027269b7acda9bc4b533298663ce
Chakib Benchinoun <chakib.ben022@gmail.com>
2022-06-10 14:16:52 +0100

    setup prettier and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ['./app/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#00b289',
+        primaryLight: '#00cc9c',
+        primaryDark: '#009975',
+      },
+      boxShadow: {
+        fullShadow: 'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;',
+      },
+    },
+  },
+  plugins: [],
+}
